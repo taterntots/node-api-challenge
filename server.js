0619606af6266eaf4a641e1b92ecceb55c807a00
taterntots <matthew.a.weidner@gmail.com>
2020-01-17 11:27:25 -0500

    created a logger middleware that logs method, url, and a timestamp anytime the server is pinged

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express'); //importing a CommonJS module
 const helmet = require('helmet'); //yarn add helmet
 const projectRouter = require('./routers/projectRouter.js'); //imports our projectRouter
 const actionRouter = require('./routers/actionRouter.js'); //imports our actionRouter
+const logger = require('./middleware/logger'); //imports my custom logger middleware
 const server = express(); //creates the server
 
 //global middleware
 server.use(express.json()); //middleware needed to parse JSON
 server.use(helmet()); //middleware that adds a layer of security to the server
+server.use(logger); //custom middleware that console logs method, url, and date on any update to the server
 
 //endpoints
 server.get('/', (req, res) => {

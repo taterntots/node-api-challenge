6f6e80ae8bed140e096bcc560be400e50d81f78b
taterntots <matthew.a.weidner@gmail.com>
2020-01-17 14:42:14 -0500

    fixed the server by adding the cors dependency to the server.js. without it, my react app axios call would not work

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); //importing a CommonJS module
 const helmet = require('helmet'); //yarn add helmet
+const cors = require('cors');
 const projectRouter = require('./routers/projectRouter.js'); //imports our projectRouter
 const actionRouter = require('./routers/actionRouter.js'); //imports our actionRouter
 const logger = require('./middleware/logger'); //imports my custom logger middleware
@@ -8,6 +9,7 @@ const server = express(); //creates the server
 //global middleware
 server.use(express.json()); //middleware needed to parse JSON
 server.use(helmet()); //middleware that adds a layer of security to the server
+server.use(cors()); //middleware that lets me access my server in my react-app
 server.use(logger); //custom middleware that console logs method, url, and date on any update to the server
 
 //endpoints

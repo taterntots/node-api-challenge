eaff2d990d55aedd16a7e6bd7d73d321016b86d3
taterntots <matthew.a.weidner@gmail.com>
2020-01-17 11:39:09 -0500

    built out some middleware to validate both Actions and Projects for required fields

---
+++ middleware/validateProject.js
@@ -0,0 +1,11 @@
+function validateProject(req, res, next) {
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ errorMessage: 'body is empty / missing project data. remember to type all the things' });
+  } else if (!req.body.name || !req.body.description) {
+    res.status(400).json({ errorMessage: 'missing name or description fields' })
+  } else {
+    next();
+  }
+}
+
+module.exports = validateProject;

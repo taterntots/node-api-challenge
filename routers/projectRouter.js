9eb9ce037918e285c171fa0f3c97c3cb4017c18a
taterntots <matthew.a.weidner@gmail.com>
2020-01-17 11:53:27 -0500

    successfully pulled a list of projects with a simple get request

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const ProjectDb = require('../data/helpers/projectModel');
 
 router.get('/', (req, res) => {
-
+  ProjectDb.get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: 'There was an error retrieving the list of projects' })
+    })
 })
+
+
 
 module.exports = router;

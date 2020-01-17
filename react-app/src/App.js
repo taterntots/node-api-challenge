7e4cf404078784d9883e4fe90c61dd899c90ab12
taterntots <matthew.a.weidner@gmail.com>
2020-01-17 13:58:45 -0500

    created a react app so I could work on getting data to display

---
+++ react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bleep Blorp</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;

06f6ede637a04946979884655348ff2d3db753c7
taterntots <matthew.a.weidner@gmail.com>
2020-01-17 12:32:42 -0500

    successfully got my posts working. can now add projects to the database, as well as actions to projects.

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const ProjectDb = require('../data/helpers/projectModel');
+const ActionDb = require('../data/helpers/actionModel');
 const validateProjectId = require('../middleware/validateProjectId');
+const validateProject = require('../middleware/validateProject');
+const validateAction = require('../middleware/validateAction');
 
 //gets a list of projects
 router.get('/', (req, res) => {
@@ -35,5 +38,30 @@ router.get('/:id/actions', validateProjectId, (req, res) => [
       res.status(500).json({ errorMessage: 'There was an error retrieving the list of actions' })
     })
 ])
+
+//adds a new project to the database
+router.post('/', validateProject, (req, res) => {
+  ProjectDb.insert(req.body)
+    .then(newProject => {
+      res.status(201).json(newProject);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: 'There was an error adding the project to the database' })
+    })
+})
+
+//adds a new action to a specified project
+router.post('/:id/actions', validateProjectId, validateAction, (req, res) => {
+  let action = req.body;
+  action = {... action, project_id: req.params.id};
+
+  ActionDb.insert(action)
+    .then(newAction => {
+      res.status(201).json(newAction);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: 'There was an error adding the action to the specified project' })
+    })
+})
 
 module.exports = router;
